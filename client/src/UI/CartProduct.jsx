85f90b7b20acb70c46e137ed030f7dc699656c3c
rakib883 <sheikhrakib883@gmail.com>
2024-08-10 01:10:57 +0600

    push

--- client/src/UI/CartProduct.jsx
+++ client/src/UI/CartProduct.jsx
@@ -24,7 +24,7 @@ const CartProduct = () => {
         }
     }, [cartData]);
 
-
+  console.log(cartData)
     // proced handeler start
     const procedHandeler = async()=>{
         try{
@@ -51,88 +51,133 @@ const CartProduct = () => {
             {cartData.length > 0 ? (
                 
                 <div className="main-area">
-                    <table className="table-auto w-full font-sans border">
-                        <thead className="bg-[#d1d5db]">
-                            <tr>
-                                <th className="text-start py-2 px-2">Product</th>
-                                <th className="py-2 px-2 text-center">Color</th>
-                                <th className="py-2 px-2 text-center">Size</th>
-                                <th className="py-2 px-2 text-center">Price</th>
-                                <th className="py-2 px-2 text-center">Quantity</th>
-                                <th className="py-2 px-2 text-center">Total</th>
-                                <th className="py-2 px-2 text-center">Remove</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {cartData.map((data) => (
-                                <tr key={data?.id} className="hover:bg-[#d1d5db]">
-                                    <td className="py-2 px-4">
-                                        <div className="main-content flex items-center gap-1">
-                                            <div className="image h-16 w-16">
-                                                <img src={data?.image} alt="" />
-                                            </div>
-                                            <div className="text">
-                                                <p>{data?.title?.substring(0, 30)}</p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td className="text-center py-2 px-4">
-                                        <div className="color flex justify-center items-center">
-                                            <div
-                                                className={`item bg-[${data?.color?.[0]}] h-5 w-5 rounded-full`}
-                                            ></div>
-                                        </div>
-                                    </td>
-                                    <td className="text-center py-2 px-4">{data?.productSize}</td>
-                                    <td className="text-center py-2 px-4">
-                                        <PrizeFormat price={data?.price} />
-                                    </td>
-                                    <td className="text-center py-2 px-4">
-                                        <IncrementDecrementButton
-                                            incrementData={() => dispatch(productIncrement({ id: data?.id }))}
-                                            decrementData={() => dispatch(productDecrement({ id: data?.id }))}
-                                            quentity={data?.quentity}
-                                            className="rounded-none py-2 border border-gray-300"
-                                        />
-                                    </td>
-                                    <td className="text-center py-2 px-4">
-                                        <PrizeFormat price={data?.quentity * data?.price} />
-                                    </td>
-                                    <td className="text-center py-2 px-4 cursor-pointer">
-                                        <button onClick={() => dispatch(singleItemRemove({ id: data?.id }))}>
-                                            <div className="item flex justify-center items-center">
-                                                <FaXmark />
-                                            </div>
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+
+                      <div className="main">
+                         <div className="table-con hidden md:block">
+                            <table className="table-auto w-full font-sans  border">
+                                <thead className="bg-[#d1d5db]">
+                                    <tr>
+                                        <th className="text-start py-2 px-2">Product</th>
+                                        <th className="py-2 px-2 text-center">Color</th>
+                                        <th className="py-2 px-2 text-center">Size</th>
+                                        <th className="py-2 px-2 text-center">Price</th>
+                                        <th className="py-2 px-2 text-center">Quantity</th>
+                                        <th className="py-2 px-2 text-center">Total</th>
+                                        <th className="py-2 px-2 text-center">Remove</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {cartData.map((data) => (
+                                        <tr key={data?.id} className="hover:bg-[#d1d5db]">
+                                            <td className="py-2 px-4">
+                                                <div className="main-content flex items-center gap-1">
+                                                    <div className="image h-16 w-16">
+                                                        <img src={data?.image} alt="" />
+                                                    </div>
+                                                    <div className="text">
+                                                        <p>{data?.title?.substring(0, 30)}</p>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className="text-center py-2 px-4">
+                                                <div className="color flex justify-center items-center">
+                                                    <div
+                                                        className={`item bg-[${data?.color?.[0]}] h-5 w-5 rounded-full`}
+                                                    ></div>
+                                                </div>
+                                            </td>
+                                            <td className="text-center py-2 px-4">{data?.productSize}</td>
+                                            <td className="text-center py-2 px-4">
+                                                <PrizeFormat price={data?.price} />
+                                            </td>
+                                            <td className="text-center py-2 px-4">
+                                                <IncrementDecrementButton
+                                                    incrementData={() => dispatch(productIncrement({ id: data?.id }))}
+                                                    decrementData={() => dispatch(productDecrement({ id: data?.id }))}
+                                                    quentity={data?.quentity}
+                                                    className="rounded-none py-2 border border-gray-300"
+                                                />
+                                            </td>
+                                            <td className="text-center py-2 px-4">
+                                                <PrizeFormat price={data?.quentity * data?.price} />
+                                            </td>
+                                            <td className="text-center py-2 px-4 cursor-pointer">
+                                                <button onClick={() => dispatch(singleItemRemove({ id: data?.id }))}>
+                                                    <div className="item flex justify-center items-center">
+                                                        <FaXmark />
+                                                    </div>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                         </div>
+                        {/* small device area start */}
+                        <div className="main my-4 md:hidden">
+                            <div className="content ">
+                               <div className="product-title flex justify-between">
+                                 <h1 className=" font-semibold font-sans">Product detils</h1>
+                                 <h1 className=" font-semibold font-sans">Price</h1>
+                               </div>
+                               <div className="product-title">
+                                 {
+                                    cartData.map((data)=>
+                                      <div key={data?.id} className="main-area my-4">
+                                          <div className="all-content flex items-center justify-between">
+                                              <div className="image">
+                                                  <div className="image flex items-center gap-1 font-sans font-semibold">
+                                                     <div className="image w-14 h-14">
+                                                        <img className="w-full h-full" src={data?.image[0]} alt="" />
+                                                     </div>
+                                                     <p>{data?.title.substring(0,20)}</p>
+                                                  </div>
+                                                  <div className="increment">
+                                                     <IncrementDecrementButton
+                                                     
+                                                     incrementData={() => dispatch(productIncrement({ id: data?.id }))}
+                                                     decrementData={() => dispatch(productDecrement({ id: data?.id }))}
+                                                     quentity={data?.quentity}
+                                                     className="w-full border border-black"/>
+                                                  </div>
+                                              </div>
+                                              <div className="price">
+                                                <PrizeFormat className="font-sans font-semibold" price={data?.price}/>
+                                              </div>
+                                          </div>
+                                      </div>
+                                    )
+                                 }
+                               </div>
+
+                            </div>
+                        </div>
+                        {/* small device area end */}
+                     </div>   
                     {/* Cart bottom area start */}
                     <div className="bottom-area my-4">
-                        <div className="main-content flex justify-between">
-                            <div className="cupon w-[25%]">
+                        <div className="main-content md:flex justify-between">
+                            <div className="cupon w-full md:w-[25%]">
                                 <div className="w-full flex items-center border">
-                                    <div className="search w-[60%]">
+                                    <div className="search w-[80%] md:w-[60%]">
                                         <input
-                                            className="px-2 border-none outline-none font-sans bg-[#f8f8f8]"
+                                            className="px-2 text-[14px] md:text-base border-none outline-none font-sans bg-[#f8f8f8]"
                                             type="text"
                                             placeholder="Discount Code"
                                         />
                                     </div>
-                                    <div className="button w-[40%] bg-black text-center">
-                                        <button className="text-white py-3 font-sans">Apply</button>
+                                    <div className="button w-[20%] md:w-[40%] bg-black text-center">
+                                        <button className="text-white md:py-3 font-sans py-3">Apply</button>
                                     </div>
                                 </div>
                             </div>
                             {/* Shipping area start */}
-                            <div className="payment w-[30%]">
-                                <div className="button flex gap-2 font-semibold font-sans text-white">
-                                    <div className="w-[60%]">
+                            <div className="payment mt-4 md:mt-0 md:w-[30%] w-full">
+                                <div className="button md:flex md:gap-2 font-semibold font-sans text-white">
+                                    <div className="md:w-[60%] w-full">
                                         <CustomButton className="w-full" title="Continue Cart" />
                                     </div>
-                                    <div className="item w-[40%]">
+                                    <div className="item md:w-[40%] mt-2 md:mt-0">
                                         <Link to="/cart">
                                             <CustomButton className="w-full" title="Update Cart" />
                                         </Link>
