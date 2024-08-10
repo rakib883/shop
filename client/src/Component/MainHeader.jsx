bd70e7000d902b35c50a100dc17f678cf5e53bcf
rakib883 <sheikhrakib883@gmail.com>
2024-08-10 15:45:07 +0600

    last

--- client/src/Component/MainHeader.jsx
+++ client/src/Component/MainHeader.jsx
@@ -57,6 +57,7 @@ function MainHeader() {
     {name :"About",path:"/about"},
     {name :"Blog",path:"/blog"},
     {name :"Contact",path:"/contact"},
+    {name :"Contact",path:"/pament-success"},
 ]
   // main mobile menu are end
 
